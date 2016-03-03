4abf6d7e2deb8c15921c362033d4fc6bdabead23
horald <horst.meyer.koeln@gmail.com>
2016-03-03 17:54:21 +0100

    undocumented Changes of Version 1.016

--- classes/about.php
+++ classes/about.php
@@ -12,6 +12,11 @@ $versnr=$ini_array['versnr'];
 $sqlite=1;
 //$versdat="21.01.2015";
 //$versnr="1.004";
+
+$ini_locarr = parse_ini_file("http://horald.github.io/joorgsqlite/version.txt");
+$neueversdat=$ini_locarr['versdat'];
+$neueversnr=$ini_locarr['versnr'];
+
 echo "<a href='../index.php' class='btn btn-primary btn-sm active' role='button'>Menü</a> "; 
 //echo "<a href='checkupdate.php?actvers=".$versnr."' class='btn btn-primary btn-sm active' role='button'>Update prüfen</a> ";
 $pfad=$_SERVER['PHP_SELF'];
@@ -28,6 +33,8 @@ echo "<pre>";
 echo "<table>";
 echo "<tr><td>Stand</td>  <td>: ".$versdat."</td></tr>";
 echo "<tr><td>Version</td><td>: ".$versnr."</td></tr>";
+echo "<tr><td>Stand</td>  <td>: ".$neueversdat."</td></tr>";
+echo "<tr><td>neue Version</td><td>: ".$neueversnr."</td></tr>";
 //echo "<tr><td>Sqlite</td><td>: ".$sqlite."</td></tr>";
 echo "<tr><td>Sourcecode unter</td><td>: <a href='https://github.com/horald/joorgsqlite' target='_blank'>github:joorgsqlite</a></td></tr>";
 echo "<tr><td>.</td> <td></td></tr>";
