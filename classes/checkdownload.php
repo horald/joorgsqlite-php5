9b52a2063dd927c6ba2bb891d836b40c52626f03
horald <horst.meyer.koeln@gmail.com>
2016-03-04 07:42:05 +0100

    undocumented Changes of Version 1.015

--- classes/checkdownload.php
+++ classes/checkdownload.php
@@ -6,7 +6,8 @@ bootstrapbegin($headline."<br>");
 echo "<a href='../index.php' class='btn btn-primary btn-sm active' role='button'>Menü</a> "; 
 echo "<a href='about.php' class='btn btn-primary btn-sm active' role='button'>About</a><br><br>"; 
 $neuevers=$_GET['neuevers'];
-echo "<a href='https://github.com/horald/joorgsqlite/tree/master/sites/update/zip/update-1.016.tar.gz' class='btn btn-primary btn-sm active' role='button'>Download Version ".$neuevers."</a> "; 
+//echo "<a href='https://github.com/horald/joorgsqlite/tree/master/sites/update/zip/update-1.016.tar.gz' class='btn btn-primary btn-sm active' role='button'>Download Version ".$neuevers."</a> "; 
+echo "<a href='https://github.com/horald/joorgsqlite/tree/gh-pages/sites/update/zip/update-1.016.tar.gz' class='btn btn-primary btn-sm active' role='button'>Download Version ".$neuevers."</a> "; 
 
 bootstrapend();
 ?>
